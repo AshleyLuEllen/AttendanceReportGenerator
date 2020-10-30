777a8fb360120c1707ce0a0fdb8b3119b451542b
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2020-10-30 16:51:31 -0500

    More small corrections to text

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ attendance_total_SI = {}
 attendance_total_SI_OH = {}
 attendance_total_SI_OH_TR = {}
 
+
 def add_emails(session):
     for email in session['attendance_records']:
         if email not in attendance_total:
@@ -29,35 +30,34 @@ def add_emails(session):
     return attendance_total
 
 
-def get_stats(countOH=False, countTR=False):
-    attendance_total = {}
-    for session in sessions_data:
-        if (session['abbreviation'] == 'SI'
-                or (session['abbreviation'] == 'OH' and countOH)
-                or (session['abbreviation'] == 'TR' and countTR)):
-            attendance_total = add_emails(session, attendance_total)
-    return attendance_total
-
-
-# return a list, and a separate function to parse this return to a csv
-def get_stats(start_date, end_date, countOH=False, countTR=False):
-    for session in sessions_data:
-        if start_date <= datetime.strptime(session['date'], "%m-%d-%Y") <= end_date:
-            if (session['abbreviation'] == 'SI'
-                    or (session['abbreviation'] == 'OH' and countOH)
-                    or (session['abbreviation'] == 'TR' and countTR)):
-                attendance_total = add_emails(session)
-
-
-def get_stats(start_date, end_date, required_count, countOH=False, countTR=False):  #TODO LOOK AT IT
-    qualified = {}
-    attendance_total = get_stats(start_date, end_date, countOH, countTR)
-    for email in attendance_total:
-        print(email, attendance_total[email])
-        if attendance_total[email] >= required_count:
-            if email not in qualified:
-                qualified[email] = attendance_total[email]
-    return qualified  # dictionary of email (key) to # sessions attended (value)
+# def get_stats(countOH=False, countTR=False):
+#     attendance_total = {}
+#     for session in sessions_data:
+#         if (session['abbreviation'] == 'SI'
+#                 or (session['abbreviation'] == 'OH' and countOH)
+#                 or (session['abbreviation'] == 'TR' and countTR)):
+#             attendance_total = add_emails(session, attendance_total)
+#     return attendance_total
+#
+#
+# # return a list, and a separate function to parse this return to a csv
+# def get_stats(start_date, end_date, countOH=False, countTR=False):
+#     for session in sessions_data:
+#         if start_date <= datetime.strptime(session['date'], "%m-%d-%Y") <= end_date:
+#             if (session['abbreviation'] == 'SI'
+#                     or (session['abbreviation'] == 'OH' and countOH)
+#                     or (session['abbreviation'] == 'TR' and countTR)):
+#                 add_emails(session)
+#
+#
+# def get_stats(start_date, end_date, required_count, countOH=False, countTR=False):  #TODO LOOK AT IT
+#     qualified = {}
+#     for email in attendance_total:
+#         print(email, attendance_total[email])
+#         if attendance_total[email] >= required_count:
+#             if email not in qualified:
+#                 qualified[email] = attendance_total[email]
+#     return qualified  # dictionary of email (key) to # sessions attended (value)
 
 
 def get_email_from_dictionary(dictionary):
@@ -85,33 +85,33 @@ def add_session(attendance, session_type, session_date):
     return sessions_data
 
 
-def get_stats_prompts():
-    print("Would you like to get stats? (Y/N)")
-    while input() == 'y' or 'Y':
-        print('Would you like to include office hours? (Y/N)')
-        office_hour = input() == 'y' or 'Y'
-        print('Would you like to include test reviews? (Y/N)')
-        test_review = input() == 'y' or 'Y'
-
-        print('Would you like to set a date range for the stats? (Y/N)')
-        response = input()
-        if response == 'y' or 'Y':
-            print('Enter the start date: (MM-DD-YYYY)')
-            date_start = input()
-            print('Enter the end date: (MM-DD-YYYY)')
-            date_end = input()
-            print('Would you like to get only students that qualified?')
-            if input() == 'y' or 'Y':
-                print('What is the number of attendances required?')
-                required_attn = int(input())
-                diction = get_stats(datetime.strptime(date_start, "%m-%d-%Y"), datetime.strptime(date_end, "%m-%d-%Y"), required_attn, office_hour, test_review)
-                print(get_email_from_dictionary(diction))
-            else:
-                diction = get_stats(datetime.strptime(date_start, "%m-%d-%Y"), datetime.strptime(date_end, "%m-%d-%Y"), office_hour, test_review)
-                print(get_email_from_dictionary(diction))
-        elif response == 'n' or 'N':
-            diction = get_stats(office_hour, False)
-            print(get_email_from_dictionary(diction))
+# def get_stats_prompts():
+#     print("Would you like to get stats? (Y/N)")
+#     while input() == 'y' or 'Y':
+#         print('Would you like to include office hours? (Y/N)')
+#         office_hour = input() == 'y' or 'Y'
+#         print('Would you like to include test reviews? (Y/N)')
+#         test_review = input() == 'y' or 'Y'
+#
+#         print('Would you like to set a date range for the stats? (Y/N)')
+#         response = input()
+#         if response == 'y' or 'Y':
+#             print('Enter the start date: (MM-DD-YYYY)')
+#             date_start = input()
+#             print('Enter the end date: (MM-DD-YYYY)')
+#             date_end = input()
+#             print('Would you like to get only students that qualified?')
+#             if input() == 'y' or 'Y':
+#                 print('What is the number of attendances required?')
+#                 required_attn = int(input())
+#                 diction = get_stats(datetime.strptime(date_start, "%m-%d-%Y"), datetime.strptime(date_end, "%m-%d-%Y"), required_attn, office_hour, test_review)
+#                 print(get_email_from_dictionary(diction))
+#             else:
+#                 diction = get_stats(datetime.strptime(date_start, "%m-%d-%Y"), datetime.strptime(date_end, "%m-%d-%Y"), office_hour, test_review)
+#                 print(get_email_from_dictionary(diction))
+#         elif response == 'n' or 'N':
+#             diction = get_stats(office_hour, False)
+#             print(get_email_from_dictionary(diction))
 
 
 def get_list_files(arg_list):
